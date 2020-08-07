1601bf2999ddbc33778957c24b6f282452029510
Vexcited <vexitofficial@gmail.com>
2020-08-07 15:01:28 +0200

    Desktop Update - v07.08.2020

---
+++ desktop/main.js
@@ -0,0 +1,60 @@
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow
+let loadingScreen;
+
+const createLoadingScreen = () => {
+  loadingScreen = new BrowserWindow(Object.assign({
+    width: 360,
+    height: 480,
+    frame: false,
+    transparent: true,
+    icon: __dirname + '/assets/icon.png'
+  }));
+  loadingScreen.setResizable(false);
+  loadingScreen.loadFile('loading/index.html');
+  loadingScreen.on('closed', () => loadingScreen = null);
+  loadingScreen.webContents.on('did-finish-load', () => {
+    loadingScreen.show();
+  });
+}
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    height: 720,
+    width: 1280,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    show: false,
+    icon: __dirname + '/assets/icon.png'
+  });
+
+  mainWindow.removeMenu()
+  mainWindow.loadURL('https://ott.vexcited.ml')
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  });
+
+  mainWindow.webContents.on('did-finish-load', () => {
+    if (loadingScreen) {
+      loadingScreen.close();
+    }
+    mainWindow.show();
+    mainWindow.maximize()
+  });
+}
+
+app.on('ready', () => {
+  createLoadingScreen();
+  createWindow();
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
