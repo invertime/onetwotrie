7d5bfaf656aa8945bae8d63855918c3b9cf21a84
Vexcited <vexitofficial@gmail.com>
2020-11-17 18:22:23 +0100

    Changing everything...

--- README.md
+++ README.md
@@ -1,51 +1,23 @@
 # OneTwoTrie, et un pas de plus vers l'écologie
+<b>Projet scolaire !</b> <br>
 Application permettant de mieux recycler ses déchets.
 Ce site reprend un peu l'idée de consignesdetri.fr de Citeo mais avec un système de code-barres.
-(À noter que ce site essaye aussi d'être le moins lourd possible !)
 Uniquement disponible en France.
-https://ott.vexcited.ml
+https://vexcited.github.io/onetwotrie/
 
 1. [Intro](https://github.com/Vexcited/onetwotrie/#onetwotrie-et-un-pas-de-plus-vers-l%C3%A9cologie)
 2. [Application PC](https://github.com/Vexcited/onetwotrie/#2-application-pc)
 3. [Application Mobile](https://github.com/Vexcited/onetwotrie/#3-application-mobile)
 4. [API (en cours)](https://github.com/Vexcited/onetwotrie/#4-api)
 
-### On a besoin d'aide !
-
-Le fichier /api/recyclage.json contient toutes les consignes de tri pour chaque villes, mais le problème c'est que nous pouvons pas tous les rajoutter à la main car
-- Nous n'avons pas le temps
-- Nous ne connaissons pas toutes les consignes de tri de chaque ville
-
-Donc si vous avez un peu de temps, merci d'essayer de remplir les consignes pour votre propre ville.
-Explications des données:
-```
-"nom_de_la_ville": {
-    "potbocalverre" => Les pots ou bocaux en verre
-    "potyaourt" => Les pots de yaourt
-    "briquealimentaire" => Les briques alimentaires (jus, lait, ...)
-    "boitemetal" => Les boîtes en métal
-    "papier" => Comme son nom, Papier
-    "bouteilleplastique" => Bouteilles en plastique
-    "bouteilleverre" => Bouteilles en verre
-    "porcelaine" => Porcelaine (assiettes, ...)
-    "boitecarton" => Boîtes en carton
-    "emballageplastique" => Emballages plastiques (peut être des étuis aussi)
-    "bouteillemetal" => Bouteilles en métal
-    "canettealuminium" => Canettes [en aluminium]
-    "paquetbiscuit" => Emballage du paquet et sachets
-}
-```
-
-Nous mentionnons tous les contributeurs à la fin de ce fichier.
-
 ## [2] Application PC
-Uniquement disponible sur Windows pour le moment.
+Uniquement disponible pour Windows (pour le moment).
 
 * [Windows - Installateur (.MSI)](https://github.com/Vexcited/onetwotrie/releases/download/desktop-v12.08.2020/setup-onetwotrie-1208202.msi)
 * [Windows - Portable (.ZIP)](https://github.com/Vexcited/onetwotrie/releases/download/desktop-v12.08.2020/portable-onetwotrie-1208202-win32-ia32.zip)
 
 ## [3] Application Mobile
-Uniquement disponible sur Android pour le moment.
+Uniquement disponible pour Android (pour le moment).
 
 * [Android (.APK)](https://github.com/Vexcited/onetwotrie/releases/download/mobile-v19.08.2020/onetwotrie-mobile-v19082020.apk)
 
---
+++ pages/assets/js/functions.js
@@ -0,0 +1,399 @@
+// Settings
+var times_i_form = 0,
+    geolocalised = false,
+    searchProductUrl = 'https://www.vexcited.ml/api/onetwotrie/searchProduct.php?q=',
+    searchCityUrl = 'https://www.vexcited.ml/api/onetwotrie/searchCity.php?q=',
+    getCityUrl = 'https://www.vexcited.ml/api/onetwotrie/getCity.php',
+    request = new XMLHttpRequest(),
+    searchField = villeField = '',
+    longitude, latitude;
+
+// Start QuaggaJS for #camera
+function startQuagga() {
+  Quagga.init({
+    inputStream : {
+      name : "Live",
+      type : "LiveStream",
+      target: document.querySelector('#livestream')
+    },
+    numOfWorkers: 8,
+    decoder: {
+      readers: [
+        {"format":"ean_8_reader", "config":{"supplements":[]}},
+        {"format":"ean_reader", "config":{"supplements":[]}},
+        {"format":"ean_reader", "config":{"supplements":['ean_5_reader', 'ean_2_reader']}}
+        ],
+    }
+  }, function(err) {
+    if (err) {
+      console.log(err);
+      return
+    }
+    Quagga.start();
+  });
+}
+
+// Search a product in #scanner's input
+$('#scanner').keyup(function(){
+  var count = 0;
+  searchField = $(this).val();
+  
+	if(searchField === '') {
+    $('#filter-records').html('');
+    $('#scanner').css('border-radius', '4px');
+		return;
+  }
+
+  $.get(searchProductUrl + $(this).val(), function(data){
+    var products = JSON.parse(data),
+			regex = new RegExp(searchField, "i"),
+      output = '';
+
+    $.each(products, function(key, val){
+      if((val.name.search(regex) != -1) || (val.brand.search(regex) != -1) || (val.barcode.search(regex) != -1)) {
+        count++;
+        if(count < 5){
+          output += '<li><a href="javascript:void(0)" onclick="changeModal(' + val.barcode + ')">' + val.brand + ' - ' + val.name + '<img class="image_search" src="' + val.image + '"></img></a></li>';
+        }
+      }
+    });
+
+  });
+  
+  $('#scanner').css('border-radius', '4px 4px 0 0');
+  $('#filter-records').html('<ul class="box">' + output + '<span style="height: 5px; display: block;"></span></ul>');
+});
+
+var recyalgeJsonHasBeenLoaded = false;
+
+function loadRecyclageJson(){
+  $.getJSON(getCityUrl, function(cities){
+    // Create new cities array
+    var newArrayCities = '[';
+    $.each(cities, function(key, val){
+      // Add every cities to this array
+      newArrayCities += '"' + key + '",';
+    });
+
+    // Remove last comma
+    newArrayCities = newArrayCities.substring(0, newArrayCities.length - 1);
+
+    // Close the array
+    newArrayCities += ']';
+    
+    // Put it into hidden input (cause it's a string actually)
+    $('#arraycities').val(newArrayCities);
+  });
+
+  // Say that it loaded the cities
+  recyalgeJsonHasBeenLoaded = true;
+}
+
+// Search a city in #localisation's input
+$('#localisation').keyup(function(){
+  if(recyalgeJsonHasBeenLoaded === false){
+    loadRecyclageJson();
+  }
+  else{
+    var cityNums = 0;
+    villeField = $(this).val(),
+    cities = JSON.parse($('#arraycities').val()),
+    citiesOut = '';
+
+    if(villeField === '') {
+      $('#filter-ville').html('');
+      $('#localisation').css('border-radius', '4px');
+      return;
+    }
+
+    for (i = 0; i < cities.length; i++) {
+      if (cities[i].substr(0, villeField.length).toUpperCase() == villeField.toUpperCase()) {
+        cityNums++;
+        if(cityNums < 8){
+          citiesOut += '<li><a href="javascript:void(0)" onclick="startInputGeo(\''+cities[i]+'\', $(\'#geoTypeBefore\').val());">' + cities[i] + '</a></li>';
+        }
+      }
+    }
+
+    $('#localisation').css('border-radius', '4px 4px 0 0');
+    $('#filter-ville').html('<ul class="boxCity box">' + citiesOut + '<span style="height: 5px; display: block;"></span></ul>');
+  }  
+});
+
+/****** - Functions -  ******/
+
+function startInputGeo (city, type){
+  $('#cityGeo').val(city);
+  geolocalised = true;
+
+  GetCity(type);
+}
+
+// Show the results if input is focused
+function show(id){
+    $(".box").fadeIn();
+    $('.box').css('display', 'block');
+
+    if($('#' + id).val() != '') {
+      $('#' + id).css('border-radius', '4px 4px 0 0');
+    }
+}
+
+// Hide the results if input is not focused
+function hide(id){
+  $(".box").fadeOut();
+    setTimeout(
+        function(){
+            $('.box').css('display', 'none');
+            $('#' + id).css('border-radius', '4px');
+        }, 250
+    );
+}
+
+function geolocalise () {
+  var type = $('#geoTypeBefore').val();
+  navigator.geolocation.getCurrentPosition(function(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    geolocalised = true;
+    GetCity(type);
+  });
+}
+
+function finalizeProduct (type, city){
+  var cityRules = JSON.parse($('#consignesCity').val());
+
+  if(type != 'autre'){
+    var consignes = cityRules[type];
+  }
+  else{
+    var consignes = 'déchetterie';
+  }
+  
+  $('#recycleItem').html('Vous recyclez à <strong>' + city + '</strong><br>Ce produit se met (dans le/à la) <strong>' + consignes + '</strong>.');
+
+  // Hide the loader (anyway)
+  $("#geoStatus").html('Géolocaliser votre appareil');
+
+  // Go to product page
+  location.hash = 'product';
+}
+
+function consignes (city, type){
+  if($('#consignesCity').val() === ''){
+    $.getJSON(getCityUrl, function(recyclage){
+      $('#consignesCity').val(JSON.stringify(recyclage[city]));
+      finalizeProduct(type, city);
+    });
+  }
+  else{
+      finalizeProduct(type, city);
+  }    
+}
+
+function GetCity (type){
+
+  // Show a loader while getJSON
+  $("#geoStatus").html('Chargement...');
+
+  if($('#cityGeo').val() === ''){
+    $.getJSON("https://api-adresse.data.gouv.fr/reverse/?lon=" + longitude + "&lat=" + latitude, function(data){
+      var item = data.features[0].properties.city;
+      $('#cityGeo').val(item);
+      consignes(item, type);
+    });
+  }
+  else{
+    consignes($('#cityGeo').val(), type);
+  }
+}
+
+
+function checkGeolocalisation (type){
+  if(geolocalised === false){
+    $('#geoTypeBefore').val(type);
+    location.hash = 'geolocalisation';
+  }
+  else if(geolocalised === true){
+    GetCity(type);
+  }
+}
+
+
+function checkAlternatives (name, alimentaire){
+  var alt_count = 0,
+      infos = '';
+
+  if(alimentaire === 'yes'){
+    alt_count++;
+    infos += '<a href="https://www.marmiton.org/recettes/recherche.aspx?type=all&aqt='+ name +'">Voir des recettes (Marmitton)</a>';
+  }
+
+  if(alt_count === 0){
+    return '<h4>Aucune alternatives à été trouvé pour ce produit.</h4>';
+  }
+  else{
+    return '<h2>Alternatives</h2>' + infos;
+  }
+}
+
+// Change page to #product when a product is clicked in `Rechercher un produit`
+function changeModal ($barcode) {
+  $.get(searchProductUrl, function(data){
+    var products = JSON.parse(data);
+
+    $.each(products, function(key, val){
+      if(val.barcode == $barcode){
+        var image_source = path + val.barcode + '.' + val.ext;
+        if($('#image_generated').length){
+          var hrefImage = $('#image_generated').attr('href');
+          if(hrefImage === image_source){
+            var newTitle = val.brand + ' - ' + val.name + ' - ' + val.barcode;
+            $('#image_generated').attr('title', newTitle);
+          }
+        }
+        else{
+          $('#product_image_span').html('<img id="image_generated" class="product_img" style="width: auto;" title="' + val.brand + ' - ' + val.name + ' - ' + val.barcode + '" src="' + image_source + '" />');
+        }
+        
+
+        var informations = '<b>Marque</b> - ' + val.brand;
+
+        informations += '<br><b>Code-barres</b> - ' + val.barcode;
+        informations += '<br><br><h2>Recyclage</h2><p id="recycleItem"></p>';
+
+        informations += checkAlternatives(val.name, val.alimentaire);
+
+              
+        $('#product_image_span').css('background-color', '#fff0');
+
+        $('#product_name').html(val.name);
+        $('#product_informations').html(informations);
+
+        checkGeolocalisation(val.type);
+        
+      }
+    });
+  });
+}
+
+
+// Open the #product article when a product is detected
+Quagga.onDetected(function(result) {
+  var $code = result.codeResult.code,
+      $quagga = 'started';
+
+  $.get(searchProductUrl, function(data){
+    var products = JSON.parse(data);
+
+    $.each(products, function(key, val) {
+      if(val.barcode == $code && $quagga != 'stopped'){
+        Quagga.stop();
+        $quagga = 'stopped';
+  
+        if($('#image_generated').length){
+          var hrefImage = $('#image_generated').attr('href');
+          if(hrefImage === val.image){
+            var newTitle = val.brand + ' - ' + val.name + ' - ' + val.barcode;
+            $('#image_generated').attr('title', newTitle);
+          }
+        }
+        else{
+          $('#product_image_span').html('<img id="image_generated" class="product_img" style="width: auto;" title="' + val.brand + ' - ' + val.name + ' - ' + val.barcode + '" src="' + val.image + '" />');
+        }
+        var informations = '<b>Marque</b> - ' + val.brand;
+  
+        informations += '<br><b>Code-barres</b> - ' + val.barcode;
+        informations += '<br><br><h2>Recyclage</h2><p id="recycleItem"></p>';
+  
+        informations += checkAlternatives(val.name, val.alimentaire);
+  
+        $('#product_name').html(val.name);
+        $('#product_informations').html(informations);
+  
+        checkGeolocalisation(val.type);
+  
+        $('#last_scanned').css('color', 'white');
+        $('#last_scanned').html('');
+        $('#status_scan').html('');
+      }
+      else{
+        if($quagga != 'stopped'){
+          $('#last_scanned').css('color', 'red');
+          $('#last_scanned').html($code);
+          $('#status_scan').html('Aucun produit n\'est attribué à ce code-barres. Veuillez réesayez.');
+        }
+      }
+    });
+  });
+});
+
+// Toggle text if checkbox is toggled in #add
+function toggleText (checkboxId, textId) {
+  var checkbox = document.getElementById(checkboxId);
+  if (checkbox.checked == true){
+    $('#' + textId).css('display', 'block');
+  }
+  else{
+    $('#' + textId).css('display', 'none');
+    $('#' + textId).val('');
+  }
+}
+
+// Form validation #add
+$('#formAdd').on('submit', function (e) {
+  times_i_form++;
+  e.preventDefault();
+
+  if(document.getElementById('checkbox_alimentaire').checked) {
+    alimentaire_check = 'yes';
+  }
+  else{
+    alimentaire_check = 'no';
+  }
+
+  if(times_i_form <= 5){
+    $.ajax({
+      type: 'POST',
+      url: 'https://www.vexcited.ml/api/onetwotrie/suggestProduct.php',
+      data: {
+        name:  $('#nom_add').val(),
+        brand:  $('#marque_add').val(),
+        barcode:  $('#codebarres_add').val(),
+        type: $('#type_add').val(),
+        alimentaire: alimentaire_check,
+        submit: 'submitted'
+      },
+      beforeSend: function() {
+        $("#load_add").html('Chargement...');
+      },
+      success: function (callback) {
+        // Reset all the form
+        $('#formAdd').trigger("reset");
+
+        // Hide the loader GIF
+        $("#load_add").html('Envoyer la demande !');
+
+        // Show the result
+        $('#status_add').html(callback);
+      }
+    });
+  }
+  else{
+    $('#status_add').html('<h4 style="color: red">Demande non envoyée car vous avez dépassé votre quota de session (5)</h4>');
+  }
+});
+
+// Electron HTML
+if($('.menubar').html()){ 
+  $('.menubar').html('');
+  $('.window-controls-container').css('position', 'absolute');
+  $('.window-controls-container').css('right', '0');
+
+  $('.window-title').css('position', 'absolute');
+  $('.window-title').css('top', '50%');
+  $('.window-title').css('left', '50%');
+  $('.window-title').css('transform', 'translate(-50%, -50%)');
+
+  $('.container-after-titlebar').css('overflow', 'auto');
+}
